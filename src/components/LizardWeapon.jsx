466fab6a8ec52c84b6d2676ed0657acba9e599d4
Martin Kutzner <mfkutz@gmail.com>
2023-06-21 19:42:06 -0300

    invert weapon desktop

--- src/components/LizardWeapon.jsx
+++ src/components/LizardWeapon.jsx
@@ -2,7 +2,7 @@ import lizard from '../assets/images/icon-lizard.svg'
 
 const LizardWeapon = () => {
     return (
-        <div className='bg-LizardA flex rounded-full absolute lg:h-[145px] h-[96px] lg:w-[145px] w-[96px] justify-center items-center lg:left-[-75px] -left-[10px] lg:top-[172px] top-[301px] hover:cursor-pointer '>
+        <div className='bg-LizardA flex rounded-full absolute lg:h-[145px] h-[96px] lg:w-[145px] w-[96px] justify-center items-center lg:left-[0px] -left-[10px] lg:top-[362px] top-[301px] hover:cursor-pointer '>
             <div className='bg-LizardB flex rounded-full absolute lg:h-[145px] h-[96px] lg:w-[145px] w-[96px] justify-center items-center -left-0 lg:-top-[6px] top-[-4px]'>
                 <div className='bg-grayShadow lg:h-[110px] h-[72px] lg:w-[110px] w-[72px] rounded-full '>
                     <div className=' bg-bgCard rounded-full lg:h-[104px] h-[67px] lg:w-[112px] w-[71px] flex justify-center items-center absolute lg:top-[25px] lg:left-[16px] top-[16px]'>
--- src/components/SpockWeapon.jsx
+++ src/components/SpockWeapon.jsx
@@ -2,9 +2,9 @@ import spock from '../assets/images/icon-spock.svg'
 
 const SpockWeapon = () => {
     return (
-        <div className='bg-SpockA flex rounded-full absolute lg:h-[145px] h-[96px] lg:w-[145px] w-[96px] justify-center items-center lg:left-[0px] -left-[55px] lg:top-[362px] top-[180px] hover:cursor-pointer '>
+        <div className='bg-SpockA flex rounded-full absolute lg:h-[145px] h-[96px] lg:w-[145px] w-[96px] justify-center items-center lg:left-[-75px] -left-[55px] lg:top-[172px] top-[180px] hover:cursor-pointer '>
             <div className='bg-SpockB flex rounded-full absolute lg:h-[145px] h-[96px] lg:w-[145px] w-[96px] justify-center items-center -left-0 lg:-top-[6px] top-[-4px]'>
-                <div className='bg-grayShadow lg:h-[72px] h-[72px] lg:w-[110px] w-[72px] rounded-full'>
+                <div className='bg-grayShadow lg:h-[110px] h-[72px] lg:w-[110px] w-[72px] rounded-full'>
                     <div className=' bg-bgCard rounded-full lg:h-[104px] h-[67px] lg:w-[112px] w-[71px] flex justify-center items-center absolute lg:top-[25px] top-[16px]'>
                         <img src={spock} alt="scissors" className='lg:w-[52px] w-[30px]' />
                     </div>
